c7aba6c66e6a8d0b59eedfa1b5741fa11b83873e
tkellehe <mitchell.kelleher@gmail.com>
2016-11-13 19:23:44 -0500

    braingolfpp
    THe starting point for the first version.

---
+++ src/braingolfpp-latest.js
@@ -0,0 +1,17 @@
+(function(global){
+
+// The current verion of braingolfpp.
+var version = "1.00.00.00",
+
+//-----------------------------------------------------------------------------
+// The main function for processing and executing the code.
+braingolfpp = function(code) {
+
+}
+
+//-----------------------------------------------------------------------------
+// Sets all of the global variables defined earlier.
+braingolfpp.version = version;
+global.braingolfpp = braingolfpp;
+
+})(this)
